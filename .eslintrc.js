5f377a17baf041cfc831580628dd8b2d345f0b14
yusuf <ybasori@gmail.com>
2023-06-10 22:47:46 +0700

    fix line break

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   plugins: ["@typescript-eslint", "react"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "windows"],
+    "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
   },
